88e430e4f6ecd2e7c9aec218240f5563300c14ca
magdalinaGe <77638615+magdalinaGe@users.noreply.github.com>
2024-04-02 19:14:40 -0600

    cooment IBookStoreRepository.cs

--- Mission11_Imerlishvili/Models/IBookStoreRepository.cs
+++ Mission11_Imerlishvili/Models/IBookStoreRepository.cs
@@ -2,6 +2,8 @@
 {
     public interface IBookStoreRepository
     {
+            // Property to access a collection of books. It returns an IQueryable<Book>, allowing consumers to query the collection.
+
         public IQueryable<Book> Books { get; }
     }
 }
